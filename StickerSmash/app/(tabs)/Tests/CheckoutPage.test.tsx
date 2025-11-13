1ea491f32cb32aee3127e25c671e2167456e1bd4
Andre Gutierrez <andregutierrez03@gmail.com>
2025-11-12 22:02:10 -0800

    I just added tests for the main pages.

---
+++ StickerSmash/app/(tabs)/Tests/CheckoutPage.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react-native';
+import CheckoutPage from './CheckoutPage';
+
+describe('CheckoutPage', () => {
+  it('renders checkout title', () => {
+    render(<CheckoutPage />);
+    expect(screen.getByText(/Checkout/i)).toBeTruthy();
+  });
+});
