ef7a25792521a5770afa5b94705bb4c55c81683a
JZavala210 <juanczavala22@gmail.com>
2025-12-01 20:28:54 -0800

    Prepare frontend for Heroku deployment

---
+++ StickerSmash/server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const buildPath = path.join(__dirname, "dist");
+
+app.use(express.static(buildPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Frontend running on port ${PORT}`);
+});
