0e36e20f293f5569a50ba3a59dcba3736349b32c
JZavala210 <juanczavala22@gmail.com>
2025-12-03 02:45:41 -0800

    Update server.js

--- StickerSmash/server.js
+++ StickerSmash/server.js
@@ -5,8 +5,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 👇 Match the Expo export output: StickerSmash/dist
-const buildPath = path.join(__dirname, "StickerSmash", "dist");
+// Expo export:web outputs to "web-build"
+const buildPath = path.join(__dirname, "web-build");
 
 // Serve static assets
 app.use(express.static(buildPath));
